fcbcd0daa943a1883778a90864dbedcbd84f7af2
Eder Medeiros <140189508+edermedeiroos@users.noreply.github.com>
2025-11-04 20:37:09 -0300

    Code working!

--- Finances.py
+++ Finances.py
@@ -7,7 +7,7 @@ with open("config.json", "r") as archive:
     configJson = json.load(archive)
 
 # Acess Credential
-SECRET = configJson["Internal Integratin Secret"]
+SECRET = configJson["Internal Integration Secret"]
 AUTH_HEADERS = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {SECRET}",
@@ -28,7 +28,7 @@ bodyDataSourceQuery = {"sorts": [
             "property": "Valor",
             "direction": "ascending"
         }]
-}
+    }
 
 # DataSource Request
 dscRequest = requests.post(url=urlDataSourceQuery, 
@@ -39,27 +39,41 @@ dscJson = dscRequest.json()
 
 # DataFrame Data
 generalData = []
-dataColumns = ["Nome", "Valor", "Tipo", "Categoria", "Data", "Valor Efetivo"]
+dataColumns = ["Nome", "Valor", "Tipo", "Categoria", "Data", "Valor Efetivo", "Conta"]
 
 # Iteraction over the pages from the list
-for object in dscJson["results"]:
-    properties = object["properties"]
+while True:
+    for object in dscJson["results"]:
+        properties = object["properties"]
 
-    # Object properties
-    name = properties["Transações"]["title"][0]["plain_text"]
-    value = properties["Valor"]["number"]
-    type = properties["Tipo"]["select"]["name"]
-    category = properties["Categoria"]["select"]["name"]
-    date = properties["Data"]["date"]["start"]
-    efectiveValue = properties["Valor Efetivo"]["formula"]["number"]
+        # Object properties
+        name = properties["Transações"]["title"][0]["plain_text"]
+        value = properties["Valor"]["number"]
+        type = properties["Tipo"]["select"]["name"]
+        category = properties["Categoria"]["select"]["name"]
+        date = properties["Data"]["date"]["start"]
+        efectiveValue = properties["Valor Efetivo"]["formula"]["number"]
+        bankAccount = properties["Conta"]["select"]["name"]
 
-    objectData = (name, value, type, category, date, efectiveValue)
+        objectData = (name, value, type, category, date, efectiveValue, bankAccount)
 
-    # Append to dataFrame
-    generalData.append(objectData)
-    print(objectData)
+        # Append to dataFrame
+        generalData.append(objectData)
+
+    # Verify if has more data
+    if dscJson["has_more"]:
+        # Cursor update for next page
+        next_cursor = dscJson["next_cursor"]
+        bodyDataSourceQuery["start_cursor"] = next_cursor
+
+        dscRequest = requests.post(url=urlDataSourceQuery, 
+                            json=bodyDataSourceQuery,
+                            headers=AUTH_HEADERS
+                            )
+        dscJson = dscRequest.json()
+    
+    else:
+        break
 
 df = pandas.DataFrame(generalData, columns=dataColumns)
-
-print(df)
 df.to_excel("Finanças.xlsx", index=False)
