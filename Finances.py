2202b2a8acf8349d2f285027fb2a8c94f7f0805d
Madeira <ederlissemedeiros@gmail.com>
2026-02-26 19:52:36 -0300

    Documentation

--- Finances.py
+++ Finances.py
@@ -4,10 +4,20 @@ import pandas
 import openpyxl
 import sqlalchemy
 
+# ----------------------------------------------------------------------------
+#             PERSONAL FINANCES - ETL
+#
+# 1) Load config json (DB & API Credentials)
+# 2) Connection to DB mysql
+# 3) Request to notion API
+# 4) While True loop to get all pages from endpoint (with validation)
+# 5) Type definition and extract items from response
+# 6) DataFrame Creation and Export (csv, xlxs and DB)
+# ----------------------------------------------------------------------------
+
 with open("config.json", "r") as archive:
     configJson = json.load(archive)
 
-# Acess Credential to DataBase + Engine
 DB_USER = configJson.get("DB_USER")
 DB_PASSWORD = configJson.get("DB_PASSWORD")
 DB_HOST = configJson.get("DB_HOST")
@@ -18,7 +28,6 @@ engine = sqlalchemy.create_engine(
     f"mysql+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 )
 
-# Acess Credential to API
 SECRET = configJson["INTERNAL_INTEGRATION_SECRET"]
 DATA_SOURCE_ID = configJson["DATA_SOURCE_ID"]
 AUTH_HEADERS = {
@@ -43,84 +52,62 @@ bodyDataSourceQuery = {"sorts": [
         }]
     }
 
-# DataSource Request
-dscRequest = requests.post(url=urlDataSourceQuery, 
-                           json=bodyDataSourceQuery,
-                           headers=AUTH_HEADERS
-                           )
+dscRequest = requests.post(url=urlDataSourceQuery, json=bodyDataSourceQuery, headers=AUTH_HEADERS)
 dscJson = dscRequest.json()
 
-# DataFrame Data
-generalData = []
-dataColumns = ["ID", "NAME", "VALUE", "TYPE", "CATEGORY", "SUB_CATEGORY", "DATE", "EFECTIVE_VALUE", "ACCOUNT"]
-
-# Primary Key Declaration
+notionData = []
+notionColumns = ["ID", "NAME", "VALUE", "TYPE", "CATEGORY", "SUB_CATEGORY", "DATE", "EFECTIVE_VALUE", "ACCOUNT"]
 index = 1
 
-# Iteraction over the pages from the list
 while True:
     for item in dscJson["results"]:
         properties = item["properties"]
         
-        # 1. Name
         try:
             name = properties["Name"]["title"][0]["plain_text"]
         except (KeyError, IndexError, TypeError):
             name = "Sem Título"
 
-        # 2. Value
         try:
             value = round(float(properties["Value"]["number"]), 2)
         except (KeyError, TypeError):
             value = 0.0
 
-        # 3. Type
         try:
             transaction_type = properties["Type"]["select"]["name"]
         except (KeyError, TypeError):
             transaction_type = None
 
-        # 4. Category
         try:
             category = properties["Category"]["select"]["name"]
         except (KeyError, TypeError):
             category = None
 
-        # 5. Sub-Category
         try:
             subCategory = properties["Sub Category"]["select"]["name"]
         except (KeyError, TypeError, AttributeError):
             subCategory = None
 
-        # 6. Date
         try:
             date = properties["Date"]["date"]["start"]
         except (KeyError, TypeError):
             date = None
 
-        # 7. Efective Value
         try:
             efectiveValue = properties["Effective Value"]["formula"]["number"]
         except (KeyError, TypeError):
             efectiveValue = 0.0
 
-        # 8. Account
         try:
             bankAccount = properties["Account"]["select"]["name"]
         except (KeyError, TypeError):
             bankAccount = None
 
         objectData = (index, name, value, transaction_type, category, subCategory, date, efectiveValue, bankAccount)
-
-        # Append to dataFrame
-        generalData.append(objectData)
-
-        # Primary Key
+        notionData.append(objectData)
         index += 1
 
-    # Verify if has more data
     if dscJson["has_more"]:
-        # Cursor update for next page
         next_cursor = dscJson["next_cursor"]
         bodyDataSourceQuery["start_cursor"] = next_cursor
 
@@ -133,9 +120,8 @@ while True:
     else:
         break
 
-df = pandas.DataFrame(generalData, columns=dataColumns)
+df_notion = pandas.DataFrame(notionData, columns=notionColumns)
 
-# Exportation
-df.to_sql(name="fat_finances", con=engine, if_exists='replace', index=False)
-df.to_excel(r"C:\BI\FinancesDB\Finanças.xlsx", index=False)
-df.to_csv(r"C:\BI\FinancesDB\FAT_TABLE.csv", index=False)
+df_notion.to_sql(name="FAT_FINANCES", con=engine, if_exists='replace', index=False)
+df_notion.to_excel(r"C:\BI\FinancesDB\Finanças.xlsx", index=False)
+df_notion.to_csv(r"C:\BI\FinancesDB\FAT_TABLE.csv", index=False)
